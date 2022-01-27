23ee1d9b5ab94524b3cd9895af9a16542290eedd
sitoff <svlsitoff@gmail.com>
2022-01-27 15:24:27 +0800

    Пояснительные комментарии

--- Stack/src/com/myedu/app/TempStack.java
+++ Stack/src/com/myedu/app/TempStack.java
@@ -1,23 +1,34 @@
 package com.myedu.app;
 
 public class TempStack <Item>{
+    //стек содержит массив обьектов ссылочного типа
     private  Item[] a;
+    //число отражающее размер массива
     private  int N;
+    //в кострукторе указываем размер массива
     public  TempStack(int cap){
         a = (Item[]) new Object[cap];
     }
+
     public boolean IsEmplty(){
         return N==0;
     }
     public int size(){
         return N;
     }
+    //При помещении элемнта в массив
+    //если его первоначальный размер превышен
+    //длина увеличивается в два раза
     public void Push(Item item){
         if(a.length==N){
             resize(2*a.length);
         }
         a[N++]=item;
     }
+    //при уменьшении размера массива
+    //крайнему элементу присваивается
+    // null и если есьвозможность дли
+    // нассива уменьшается в два раза
     public Item pop(){
         Item item = a[--N];
         a[N]=null;
