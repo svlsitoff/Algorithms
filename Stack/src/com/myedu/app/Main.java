a9560cf8746c2b204fd608665264791e7a094a9f
sitoff <svlsitoff@gmail.com>
2022-01-27 14:58:16 +0800

    Применение стека

--- Stack/src/com/myedu/app/Main.java
+++ Stack/src/com/myedu/app/Main.java
@@ -5,16 +5,14 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
         Random random = new Random();
         TempStack<Integer> a;
         a = new TempStack<Integer>(100);
         for (int i=0;i<120;i++){
-            int ranInt = random.nextInt(1000);
+            int ranInt = random.nextInt(100000);
             a.Push(ranInt);
         }
         for(int j =0;j<a.size();j++) {
-            int item = a.pop();
             System.out.println(a.pop());
         }
     }
